6e549737407a1478b6cf66dc87691d92be467d63
Astha Kumari <astha.kri50@gmail.com>
2022-07-20 19:12:17 +0530

    backend implemented

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,38 @@
-const Express = require('express') ;
+const Express = require('express');
+const Router = require('./routes');
+const { sequelize  , Word} = require('./models');
 
-const Router = require('./routes') ;
+async function initialize() {
+    const app = Express();
 
-const app = Express();
+    app.use(Express.json());
+    app.use("/api", Router);
 
-app.use(Express.json() );
-app.use("/api" , Router) ;
+    await sequelize.sync();
+    // await Word.bulkCreate([
+    //     {
+    //         title: "bazzinga"
+    //     },
+    //     {
+    //         title: "amazing"
+    //     },
+    //     {
+    //         title: "scaler"
+    //     },
+    //     {
+    //         title: "nodejs"
+    //     },
+    //     {
+    //         title: "programme"
+    //     },
+    //     {
+    //         title: "hangman"
+    //     }
+    // ])
 
-app.listen(8000 , ()=>{
-    console.log("Running application on port 8000");
-})
+    app.listen(8000, () => {
+        console.log("Running application on port 8000");
+    })
+}
+
+initialize();
--- backend/routes/sessions/controller.js
+++ backend/routes/sessions/controller.js
@@ -1,31 +1,38 @@
-function CreateSession(req , res) {
-    const name = req.body.name ;
+const Sequelize = require('sequelize') ;
 
+const { Word , gameSession, GameSession} = require('../../models') ;
+const serializeGameSession = require('../../serializers/gameSession') ;
+const gameSessionService = require('../../services/game_session_service') ; 
+
+
+async function CreateSession(req , res) {
+    const name = req.body.name ;
+    const word = await Word.findOne({
+        order: Sequelize.fn("RANDOM") 
+    }
+    ) ;
+    //select * from words order by random() limit 1 ;
+    const gameSession = await GameSession.create({
+        playedname : name,
+        playedLetters: "",
+        wordId: word.id,
+        startedAt: new Date()
+    })
     //TODO : use this name
 
-    res.json({
-        id: "123",
-        livesLeft : 6,
-        result: false ,
-        maskedWord: ["_","_"]
-    })
+    res.json(await serializeGameSession(gameSession)) ;
 }
 
 
-function PlaySession(req , res) {
+async function PlaySession(req , res) {
     const gameId = req.params.id ;
     const letter = req.body.letter ;
-
+    const gameSession = await GameSession.findByPk(gameId) ;
     //TODO : use this letter
-    console.log(gameId) ;
-    console.log(letter) ;
+   
+    await gameSessionService.playWordInGameSession(gameSession , letter)  ;
 
-    res.json({
-        id: gameId,
-        livesLeft : 6,
-        result: false ,
-        maskedWord: ["_","_"]
-    })
+    res.json(await serializeGameSession(gameSession)) ;
 }
 
 module.exports = {
